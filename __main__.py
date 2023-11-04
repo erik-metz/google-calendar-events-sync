7a4e8cf72de0468a6c08ac262554583c0608edd7
Erik Metz <erik.metz.info@gmail.com>
2023-11-04 14:56:07 +0100

    add main

--- __main__.py
+++ __main__.py
@@ -45,3 +45,7 @@ class Item(BaseModel):
 async def create_item(item: Item):
     items.append(item)
     return item
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
