ef98a8ca1f3f4817eee21199cc3140dd96cb9838
Erik Metz <erik.metz.info@gmail.com>
2023-11-04 15:53:28 +0100

    added google subscription

--- main.py
+++ main.py
@@ -6,35 +6,35 @@ from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 from pydantic import BaseModel
 
-# from googleCalendar import subscribe_to_google_calendar_push_notifications
+from googleCalendar import subscribe_to_google_calendar_push_notifications
 
 load_dotenv()
 
 app = FastAPI()
-# subscribe_to_google_calendar_push_notifications()
+subscribe_to_google_calendar_push_notifications()
 
 @app.get("/")
 async def root():
     return RedirectResponse(url=os.environ.get("REDIRECT_URL"))
 
-# Create a Pydantic model to define the expected data in the POST request
-class Item(BaseModel):
-    name: str
-    description: str = None
+# # Create a Pydantic model to define the expected data in the POST request
+# class Item(BaseModel):
+#     name: str
+#     description: str = None
 
-# In-memory storage for demonstration purposes
-items = []
+# # In-memory storage for demonstration purposes
+# items = []
 
-# Define a POST endpoint to create an item
-@app.post("/items/", response_model=Item)
-async def create_item(item: Item):
-    items.append(item)
-    return item
+# # Define a POST endpoint to create an item
+# @app.post("/items/", response_model=Item)
+# async def create_item(item: Item):
+#     items.append(item)
+#     return item
 
-# Define a GET endpoint to retrieve all items (for testing)
-@app.get("/items/", response_model=List[Item])
-async def read_items():
-    return items
+# # Define a GET endpoint to retrieve all items (for testing)
+# @app.get("/items/", response_model=List[Item])
+# async def read_items():
+#     return items
 
 class Google_Calendar_Event(BaseModel):
     name: str
